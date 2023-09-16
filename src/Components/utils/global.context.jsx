95bbbf6d6cae62c3cb403c4bc90fefbb4e4c5a38
Jose Martinez <jdm.bass89@gmail.com>
2023-09-16 00:27:02 -0300

    cambios sin guardar

--- src/Components/utils/global.context.jsx
+++ src/Components/utils/global.context.jsx
@@ -1,19 +1,16 @@
 import React, { createContext, useReducer, useContext, useEffect } from "react";
 import axios from "axios";
 
-// Estado inicial del contexto global
+
 export const initialState = {
-  theme: "light", // Tema por defecto 'light'
+  theme: "light", 
   dataFavs: [],
   dataApi: [],
 };
 
-// Creación del contexto global
+
 const ContextGlobal = createContext(undefined); 
 
-/* Definimos el reductor que gestionará el estado global
-...state : Esto crea una copia del estado actual (state) para mantener la inmutabilidad del estado
-action.payload: valor que se pasa como argumento cuando se despacha una acción en el reductor*/
 const globalReducer = (state, action) => {
   switch (action.type) {
     case "SET_THEME":
@@ -27,27 +24,24 @@ const globalReducer = (state, action) => {
   }
 };
 
-/*Componente ContextProvider que proporciona el contexto global a la aplicación
--state: Esta constante almacena el estado actual de la aplicación. 
-Inicialmente, tiene el valor que se proporciona como segundo argumento a useReducer (initialState)
--dispatch es la función que se utiliza para disparar acciones 
-que luego son procesadas por (globalReducer) para actualizar el estado global de la aplicación.*/
-export const ContextProvider = ({ children }) => {
-  const [state, dispatch] = useReducer(globalReducer, initialState); //useReducer gestiona el estado global
 
-  // Al cargar el componente, obtiene el tema del localStorage
+export const ContextProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(globalReducer, initialState); 
+
+  
   useEffect(() => {
     const themeStored = localStorage.getItem("theme");
     if (themeStored) {
       dispatch({ type: "SET_THEME", payload: themeStored });
     }
-    // Obtener los favoritos almacenados en localStorage y establecerlos en el estado
+    
     const favoritesStored = localStorage.getItem("favorites");
     if (favoritesStored) {
       const favorites = JSON.parse(favoritesStored);
       dispatch({ type: "SET_DATA_FAVS", payload: favorites });
     }
-    //Leer la api
+  
+
     try {
       axios
         .get("https://jsonplaceholder.typicode.com/users")
@@ -59,13 +53,13 @@ export const ContextProvider = ({ children }) => {
     }
   }, []);
 
-  // Función para cambiar el tema del contexto global y actualizar el localStorage
+ 
   const setTheme = (theme) => {
-    localStorage.setItem("theme", theme); // Guardo el tema en localStorage para futuras sesiones
-    dispatch({ type: "SET_THEME", payload: theme }); // Esto me actualiza el tema en el estado global
+    localStorage.setItem("theme", theme); 
+    dispatch({ type: "SET_THEME", payload: theme }); 
   };
 
-  // Acción para establecer los datos (cambiar)
+ 
   const setDataFavs = (dataFavs) => {
     dispatch({ type: "SET_DATA_FAVS", payload: dataFavs });
   };
@@ -87,7 +81,6 @@ export const ContextProvider = ({ children }) => {
       {children}
     </ContextGlobal.Provider>
   );
-}; //Aqui termina mi ContextProvider
+}; 
 
-// Hook personalizado para acceder al contexto global
 export const useContextGlobal = () => useContext(ContextGlobal);
